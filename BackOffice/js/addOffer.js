d6c12db87d79b036bf6e2e56b77161eabbea9000
ameni <ameni.hajri@esprit.tn>
2024-10-04 15:12:59 +0100

    first commit

--- BackOffice/js/addOffer.js
+++ BackOffice/js/addOffer.js
@@ -1,37 +1,37 @@
-// Partie 3 Etude de cas 
-
 document.addEventListener("DOMContentLoaded",function(){
 
-var titleElement=document.getElementById("title");
-var destinationElement=document.getElementById("destination");
+    var titleElement=document.getElementById("title");
+    var destinationElement=document.getElementById("destination");
 
-titleElement.addEventListener("keyup",function(){
-    var titleValue=titleElement.value;
-    var titleError=document.getElementById("title_error");
-
-    if(titleValue.length<3){
-        titleError.innerHTML="Le titre doit contenir au moins 3 caractères";
-        titleError.style.color="red";
-    }
-    else {
-        titleError.innerHTML="Correct";
-        titleError.style.color="green";
-    }
-
-    destinationElement.addEventListener("keyup",function(){
-        var destinationValue=destinationElement.value;
-        var destinationError=document.getElementById("destination_error");
-        var pattern= /^[A-Za-z\s]{3,}$/;
-
-        if(!pattern.test(destinationValue)){
-            destinationError.innerHTML="La destination doit contenir uniquement des lettre et des espaces";
-            destinationError.style.color="red";
+    titleElement.addEventListener("keyup",function(){
+        var title=titleElement.value;
+        var title_error=document.getElementById("title_error");
+        if(title.length<3){
+            title_error.innerHTML="Le titre doit contenir au moins 3 caractères";
+            title_error.style.color="red";
         }
-        else {
-            destinationError.innerHTML="Correct";
-            destinationError.style.color="green";
+        else{
+            title_error.innerHTML="Correct";
+            title_error.style.color="green";
         }
-        })
     })
+    destinationElement.addEventListener("keyup",function(){
+    var destination=destinationElement.value;
+    var destination_error=document.getElementById("destination_error");
+    var pattern=/^[A-Za-z\s]{3,}$/;
 
+    if (pattern.test(destination)==false){
+        destination_error.innerHTML="La destination ne doit pas avoir des caractères spéciaux et doit contenir au moins 3 caractères";
+        destination_error.style.color="red";
+    }
+    else{
+        destination_error.innerHTML="Correct";
+        destination_error.style.color="green";
+    }
+    })
 })
+
+
+
+
+
